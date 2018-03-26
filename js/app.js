113f9d0ba795d42bf2302de2c9a60af29ad3d285
Marc Aaron <marc.aaron.glasser@gmail.com>
2018-03-25 19:35:26 -0700

    fix rAF

--- js/app.js
+++ js/app.js
@@ -195,52 +195,22 @@ function initMeshes(){
 			// // window.addEventListener( 'mousemove', onMousemove, false );
 			// window.addEventListener( 'mousedown', onMousedown, false );
 
-			// rAF fix via Addy Osmani
-
-			var limitLoop = function (fn, fps) {
-
-			    // Use var then = Date.now(); if you
-			    // don't care about targetting < IE9
-			    var then = new Date().getTime();
-
-			    // custom fps, otherwise fallback to 60
-			    fps = fps || 60;
-			    var interval = 1000 / fps;
-
-			    return (function loop(time){
-			        requestAnimationFrame(loop);
-
-			        // again, Date.now() if it's available
-			        var now = new Date().getTime();
-			        var delta = now - then;
-
-			        if (delta > interval) {
-			            // Update time
-			            // now - (delta % interval) is an improvement over just
-			            // using then = now, which can end up lowering overall fps
-			            then = now - (delta % interval);
-
-			            // call the fn
-			            fn();
-			        }
-			    }(0));
+			var animate = function () {
+				setTimeout(function(){
+					object.rotation.x += 0.01;
+					object.rotation.y += 0.01;
+					object2.rotation.x += 0.01;
+					object2.rotation.y += 0.01;
+					object3.rotation.x += 0.01;
+					object3.rotation.y += 0.01;
+					object4.rotation.x += 0.01;
+					object4.rotation.y += 0.01;
+					renderer.render(scene, camera);
+					requestAnimationFrame(animate);
+				}, 1000/60);
 			};
 
-
-			var fn = function () {
-				object.rotation.x += 0.01;
-				object.rotation.y += 0.01;
-				object2.rotation.x += 0.01;
-				object2.rotation.y += 0.01;
-				object3.rotation.x += 0.01;
-				object3.rotation.y += 0.01;
-				object4.rotation.x += 0.01;
-				object4.rotation.y += 0.01;
-				renderer.render(scene, camera);
-			};
-
-			limitLoop(fn, 60);
-
+			animate();
 
 			let size = 0.1;
 			let grow = true;
